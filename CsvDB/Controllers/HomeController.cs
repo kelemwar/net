16d9f95701b168741d73c22eb18ca6ebde7dd429
Igor Pavlenko <pavlenko.i.o@gmail.com>
2018-06-11 10:58:56 +0300

    11.06

--- CsvDB/Controllers/HomeController.cs
+++ CsvDB/Controllers/HomeController.cs
@@ -3,28 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using LINQtoCSV;
+using CsvDB.Models;
+using CsvHelper;
 
 namespace CsvDB.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDBEntities db = new MyDBEntities();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase upload)
+        {
+
+            string path = null;
+            List<Products> productsList = new List<Products>();
+            try
+            {
+                if (upload.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(upload.FileName);
+                    path = AppDomain.CurrentDomain.BaseDirectory + "upload\\" + fileName;
+                    upload.SaveAs(path);
+
+                    var csv = new CsvReader(new StreamReader(path));
+                    csv.Read();
+                    csv.ReadHeader();
+                    var header = csv.Context.HeaderRecord;
+                    var producParse = csv.GetRecords<Products>();
+
+
+
+                    foreach (var c in producParse)
+                    {
+                        Products product = new Products();
+
+                        product.SKU = c.SKU;
+                        product.Manufacture = c.Manufacture;
+                        product.Cost = c.Cost;
+                        product.Weight = c.Weight;
+                        product.Size = c.Size;
+                        product.Title = c.Title;
+                        product.Shipping = c.Shipping;
+                        product.Color = c.Color;
+                        product.Description = c.Description;
+
+
+                        productsList.Add(product);
+                    }
+                    
+
+                     ViewBag.Header = header;
+                }   
+            }
+            catch 
+            {
+
+                ViewData["Error"] = "Upload filed";
+            }
+
+            String val = Request.Form["mapSelect"];
+            
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+               
     }
 }
---
+++ CsvDB/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CsvDB.Models
+{
+    public class Products
+    {
+        public string SKU { get; set; }
+        public string Manufacture { get; set; }
+        public string Cost { get; set; }
+        public string Weight { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string Shipping { get; set; }
+
+    }
+}
